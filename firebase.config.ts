9a1bd1f011aefe2b2ab98b06446257b3165eacc7
Krystian Lisowski <lisowski@nonstop.co>
2024-02-12 12:06:10 +0100

    Add firebase config

---
+++ firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  projectId: 'vocapp-55740',
+  appId: '1:665105657279:web:46d54e129bca6640ce050a',
+  storageBucket: 'vocapp-55740.appspot.com',
+  apiKey: 'AIzaSyDJRVlnHdrUJTatcw6tLL5FsrWqL9wiZv8',
+  authDomain: 'vocapp-55740.firebaseapp.com',
+  messagingSenderId: '665105657279',
+};
